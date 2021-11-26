721e91de6cd9c9a0a5ede66de2a87ea3e88bd0df
CGuerra2021 <alu0100703535@ull.edu.es>
2021-11-26 12:40:12 +0000

    new-deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Primer post",
+        "excerpt":" Hello mates!    This is mi first post   About:  Im Carlos Guerra and im studying to be a teacher.  Also i am a good fisherman.       Tabla:                  Tarea       Practica                       J. Web       Github                 In process       En proceso                           ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2021/11/04/aete",
+        "teaser": null
+      },{
+        "title": "Second Post",
+        "excerpt":"Second post reference MDL     Keep fishing  Whe are  so so lost but we keep fishing   \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\]  Today is tuesday, we get a whale but we still need fishing….   Me gustan las/los naranjas   Me gustan las/los melones   Me gustan las/los pescado  ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/2021/11/09/PostII",
+        "teaser": null
+      },{
+        "title": "Third Educational Post",
+        "excerpt":"                             First name                Last name                Age                Location                              John            Doe            35            United States                           Jane            Doe            29            France                           Jack            Hill            25            Australia                                                                          ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/2021/11/11/clase",
+        "teaser": null
+      },{
+        "title": "Adaptar es integrar",
+        "excerpt":"Que tipos de adaptaciones podemos realizar para lograr integrar a todos los alumnos dentro del centro, de la clase de un grupo….. Se podria y deberia enfocar desde diferentes aspectos: Adaptaciones en el CENTRO: Eliminar las barreras arquitectónicas mediante el uso de rampas en lugares de acceso vital al centro....","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/2021/11/13/Adaptaciones",
+        "teaser": null
+      },{
+        "title": "Procedimientos de integracion a alumnos con NEAE",
+        "excerpt":"Los niños en la escuela, al mismo tiempo que adquieren nuevos conocimientos, desarrollan su carácter, su capacidad de reflexión e interacción con otras personas. En la sociedad la escuela funciona como un espacio socializador cuyo objetivo es brindar igualdad de oportunidades para todos los estudiantes. Para que todas estas funciones...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/2021/11/24/NEAE",
+        "teaser": null
+      },{
+        "title": "Aplicacion de las TIC",
+        "excerpt":"En los últimos años, las tecnologías han cambiado nuestros hábitos en muchas de las facetas de nuestra vida y esas transformaciones son cada vez más rápidas. Uno de los ámbitos en los que mas han influido es en la educación y sus respectivos procesos de aprendizaje tanto individual como en...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/2021/11/24/TIC",
+        "teaser": null
+      },{
+        "title": "Ventajas de las TIC segun los profesores",
+        "excerpt":"Un estudio llevado a cabo por Samsung en 2016 entre profesores españoles sobre el uso de la tecnología en el aula, muestra interesantes conclusiones sobre los beneficios que el uso de la tecnología en la educación puede tener en los colegios. “El 84% de los encuestados percibe un aumento en...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/2021/11/24/TIC2",
+        "teaser": null
+      },{
+        "title": "Proyecto Full Store",
+        "excerpt":"En este trabajo pretendemos generar un codigo que automatice el aforo de una tienda. Para ello utilizaremos el gitbloq donde podremos programar por bloques, haciendo la practica dinámica y accesible para el alumnado. El proceso se consiste en que al pasar por la puerta nuestro sensor detecta la entrada o...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/2021/11/26/PracticaPandoc",
+        "teaser": null
+      }]
